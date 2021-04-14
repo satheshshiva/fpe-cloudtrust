abb32c2397693dbe3a091b02ef8c242fd3ab7f9c
Sathesh Sivashanmugam <sathesh.sivashanmugam@aexp.com>
2021-04-14 15:39:35 -0700

    fix a bug

--- ff1_export.go
+++ ff1_export.go
@@ -14,122 +14,50 @@ const blockSizeFF1 = 16
 //export Encrypt
 func Encrypt(_pt *C.char, _key *C.char, _tweak *C.char) *C.char {
 	pt := C.GoString(_pt)
-	key := C.GoString(_key)
-	tweak := C.GoString(_tweak)
 	generic := format.NewGenericPIIFormat()
-	radix := len(generic.CharToInt)
-	var toBeEncCnt int
-	cs := generic.CharToInt
-	toBeEnc := make([]uint16, len(pt))
-	outliers := make(map[int]string)
-
-	//form the outlying characters map
-	for i, val := range pt {
-		v := string(val)
-		if j, ok := cs[v]; ok {
-			toBeEnc[toBeEncCnt] = j
-			toBeEncCnt++
-		} else if generic.SkipOutliers {
-			outliers[i] = v
-		} else {
-			panic(fmt.Sprintf("Value %s not in characterset", v))
-		}
-	}
-	//trim the to Enc. the outliers would have left blanks
-	toBeEnc = toBeEnc[0:toBeEncCnt]
-	//the actual encryption params
-	ke := []byte(key)
-	twk := []byte(tweak)
-	r := uint32(radix)
+	ke := []byte(C.GoString(_key))
+	twk := []byte(C.GoString(_tweak))
+	r := uint32(len(generic.CharToInt))
 	encrypter, err := getFF1Encrypter(ke, twk, r)
 	if err != nil {
 		panic("couldn't create FF1 encrypter " + err.Error())
 	}
-
-	//TODO is the numeral string conversion needed
-	var src = fpe.NumeralStringToBytes(toBeEnc)
-	var dst = make([]byte, len(src))
-	//Encryption
-	encrypter.CryptBlocks(dst, src)
-	var tempCt = fpe.BytesToNumeralString(dst)
-	var ct string
-	// forming the cipher text from encrypted numerals
-	//the encrypted will have numeral string. replace the integers with actual characters from character set
-	for i, val := range tempCt {
-		//For this current position check whether outlying character exists
-		if _, ok := outliers[i]; ok {
-			ct += outliers[i]
-		}
-		ct += generic.IntToChar[val]
-	}
-	return C.CString(ct)
+	cipherText := format.Transform(pt, encrypter, generic)
+	return C.CString(cipherText)
 }
 
 //export Decrypt
 func Decrypt(_ct *C.char, _key *C.char, _tweak *C.char) *C.char {
-	ct := C.GoString(_ct)
-	key := C.GoString(_key)
-	tweak := C.GoString(_tweak)
+	pt := C.GoString(_ct)
 	generic := format.NewGenericPIIFormat()
-
-	radix := len(generic.CharToInt)
-	var toBeEncCnt int
-	cs := generic.CharToInt
-	toBeEnc := make([]uint16, len(ct))
-	outliers := make(map[int]string)
-
-	for i, val := range ct {
-		v := string(val)
-		if j, ok := cs[v]; ok {
-			toBeEnc[toBeEncCnt] = j
-			toBeEncCnt++
-		} else if generic.SkipOutliers {
-			outliers[i] = v
-		} else {
-			panic(fmt.Sprintf("Value %s not in characterset", v))
-		}
-	}
-
-	//trim the to Enc. the outliers would have left blanks
-	toBeEnc = toBeEnc[0:toBeEncCnt]
-
-	//the actual encryption params
-	ke := []byte(key)
-	twk := []byte(tweak)
-	r := uint32(radix)
-	decrypter, err := getFF1Decrypter(ke, twk, r)
+	ke := []byte(C.GoString(_key))
+	twk := []byte(C.GoString(_tweak))
+	r := uint32(len(generic.CharToInt))
+	encrypter, err := getFF1Decrypter(ke, twk, r)
 	if err != nil {
-		panic("couldn't create FF1 decrypter " + err.Error())
+		panic("couldn't create FF1 encrypter " + err.Error())
 	}
-
-	//TODO is the numeral string conversion needed
-	var src = fpe.NumeralStringToBytes(toBeEnc)
-	var dst = make([]byte, len(src))
-	//Encryption
-	decrypter.CryptBlocks(dst, src)
-	var tempPt = fpe.BytesToNumeralString(dst)
-	var pt string
-	// forming the cipher text from encrypted numerals
-	//the encrypted will have numeral string. replace the integers with actual characters from character set
-	for i, val := range tempPt {
-		//For this current position check whether outlying character exists
-		if _, ok := outliers[i]; ok {
-			pt += outliers[i]
-		}
-		pt += generic.IntToChar[val]
-	}
-	return C.CString(pt)
-
+	cipherText := format.Transform(pt, encrypter, generic)
+	return C.CString(cipherText)
 }
 
 func main() {
-	pt := "abcdefghijkl"
+	//pt := ";kmfglskdnfg v;df123m sdlafkmlkdsf;lke456jr sfdlmsf;lnefnewr;f742knmerf"
+	pt := ";kmfglskdnfg v;df123m sdla✅fkmlkdsf;lke456jr sfdl✅msf;lnefnewr;f742knmerf" //fail
+	//pt := "✅ke456jr sfdlmsf"	//fail
 	fmt.Println(pt)
 	ct := Encrypt(C.CString(pt), C.CString("y9zHShe/o7I5jFa41JMEFA=="), C.CString("39383736353433323130"))
-	fmt.Println(C.GoString(ct))
+	cipherText := C.GoString(ct)
+	fmt.Println(cipherText)
 
 	decrypted := Decrypt(ct, C.CString("y9zHShe/o7I5jFa41JMEFA=="), C.CString("39383736353433323130"))
-	fmt.Println(C.GoString(decrypted))
+	_decrypted := C.GoString(decrypted)
+	fmt.Println(_decrypted)
+	if pt == _decrypted {
+		fmt.Println("INPUT = DECRYPTED")
+	} else {
+		panic("!INPUT NOT DECRYPTED")
+	}
 	//TestEncryptDecrypt()
 }
 
--- fpe/format/transform.go
+++ fpe/format/transform.go
@@ -1,5 +1,61 @@
 package format
 
-func transform() {
+import "C"
+import (
+	"crypto/cipher"
+	"fmt"
+	"github.com/cloudtrust/fpe/fpe"
+)
 
+// Transform Encrypts or Decypts the given string based on the given fpe format
+// Three steps involved
+// Step 1: Remove the outlying characters and form the outlying characters map and toBeProcessed array
+// Step 2: Encrypt or Decypt the toBeProcessed array
+// Step 3: Combine both the processed array and outliers map
+
+func Transform(ip string, encdec cipher.BlockMode, fpeformat *format) string {
+	var toBeEncCnt int
+	cs := fpeformat.CharToInt
+	toBeProcessed := make([]uint16, len(ip))
+	outliers := make(map[int]string)
+	ipRune := []rune(ip)
+
+	// Step 1: Remove the outlying characters and form the outlying characters map and toBeProcessed array
+	for i := 0; i < len(ipRune); i++ {
+		v := string(ipRune[i])
+		if j, ok := cs[v]; ok {
+			toBeProcessed[toBeEncCnt] = j
+			toBeEncCnt++
+		} else if fpeformat.SkipOutliers {
+			outliers[i] = v
+		} else {
+			panic(fmt.Sprintf("Value %s not in characterset", v))
+		}
+	}
+
+	// Step 2: Encrypt or Decypt the toBeProcessed array
+	//trim the to Enc. the outliers would have left blanks
+	toBeProcessed = toBeProcessed[0:toBeEncCnt]
+	//TODO is the numeral string conversion needed
+	var src = fpe.NumeralStringToBytes(toBeProcessed)
+	var dst = make([]byte, len(src))
+	//Encryption
+	encdec.CryptBlocks(dst, src)
+	var tempOp = fpe.BytesToNumeralString(dst)
+	var op string
+
+	// Step 3: Combine both the processed array and outliers map
+	// forming the cipher text from encrypted numerals
+	//the encrypted will have numeral string. replace the integers with actual characters from character set
+	j := 0
+	for i := 0; i < len(tempOp)+len(outliers); i++ {
+		//For this current position check whether outlying character exists
+		if _, ok := outliers[i]; ok {
+			op += outliers[i]
+		} else {
+			op += fpeformat.IntToChar[tempOp[j]]
+			j++
+		}
+	}
+	return op
 }
